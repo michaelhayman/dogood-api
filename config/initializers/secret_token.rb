a4c4109cfa99b88dee54a7319fdbe8868d3ac030
Michael Hayman <michael.hayman@zoocasa.com>
2013-08-07 06:38:20 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoGoodApp::Application.config.secret_key_base = '1e63926f6f7c6f7a9062491e5ad28e30136df734764106c6264422a444ddac51d02dda69a32188c863167ccfecbeba61007cbeada86ebd055a64ccf156cfcec7'
