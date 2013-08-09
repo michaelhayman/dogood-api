39a74a75c7ee44e195daf73e8998397dc590f770
Michael Hayman <michael.hayman@zoocasa.com>
2013-08-09 02:51:11 -0400

    Add default format for index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 DoGoodApp::Application.routes.draw do
+
   get "category/index"
   devise_for :users
 
   resources :goods
-  root to: 'goods#index'
+
+  root to: 'goods#index', :defaults => { :format => :json }
 
   resources :users
 end
