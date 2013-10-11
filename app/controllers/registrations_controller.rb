ca88becc8b52a7ec092d8c0e1833143edcfe376d
Michael Hayman <michael.hayman@zoocasa.com>
2013-10-11 13:30:49 -0400

    Check some attributes in this controller.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   respond_to :json
+  before_filter :check_auth, :only => [ :update ]
 
   def create
     user = User.new(resource_params)
