6bbcbbdcc758846c3ebfc5acd33f6207a7889f0b
Michael Hayman <michael.hayman@zoocasa.com>
2013-10-22 13:54:26 -0400

    Break method onto multiple lines.

--- app/models/claimed_reward.rb
+++ app/models/claimed_reward.rb
@@ -4,7 +4,13 @@ class ClaimedReward < ActiveRecord::Base
   scope :recent, -> { order('created_at DESC') }
 
   def withdraw_points
-    Point.record_points("ClaimedReward", self.id, "Claim", self.user_id, nil, - self.reward.cost)
+    Point.record_points(
+      "ClaimedReward",
+      self.id,
+      "Claim",
+      self.user_id,
+      nil,
+      - self.reward.cost)
   end
 
   def refund_points
