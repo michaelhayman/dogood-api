df7e88313f744091579a08ee3efd69090255e5a1
Michael Hayman <michael.hayman@zoocasa.com>
2014-01-10 16:02:44 +1100

    Customize serialized nominee output.

--- app/models/nominee.rb
+++ app/models/nominee.rb
@@ -8,3 +8,16 @@ class Nominee < ActiveRecord::Base
     length: { maximum: 40, message: "Please enter a shorter name." }
 end
 
+class NomineeSerializer < ActiveModel::Serializer
+  attributes :full_name,
+    :avatar,
+    :email,
+    :phone,
+    :user_id,
+    :twitter_id,
+    :facebook_id
+
+  def avatar
+    object.avatar.url
+  end
+end
