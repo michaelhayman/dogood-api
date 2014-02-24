84319e110ad4080c71e3a3071df5412dc0f2adc5
Michael Hayman <michael.hayman@zoocasa.com>
2014-02-24 05:04:54 -0500

    Finish good decorator.

--- app/decorators/good_decorator.rb
+++ app/decorators/good_decorator.rb
@@ -7,22 +7,8 @@ class GoodDecorator < Draper::Decorator
   delegate_all
 
   def current_user_commented
-    helpers.dg_user.listing_saved?(object)
+    helpers.dg_user.good_commented?(object)
   end
   memoize :current_user_commented
-
-  # def good_commented?(good_or_good_id)
-  #   if object.present?
-  #     id = case good_or_good_id
-  #       when Good
-  #         good_or_good_id.id
-  #       else
-  #         good_or_good_id
-  #     end
-
-  #     !!self.commented_good_ids.include?(id)
-  #   end
-  # end
-  # memoize :good_commented?
 end
 
