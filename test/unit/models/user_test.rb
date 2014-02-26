1ed194283f6d3cb099d6773dea5cc7e278be0141
Michael Hayman <michael.hayman@zoocasa.com>
2014-02-26 03:19:27 -0500

    Convert User specs to tests.

--- test/unit/models/user_test.rb
+++ test/unit/models/user_test.rb
@@ -7,4 +7,50 @@ class UserTest < DoGood::TestCase
       assert user.valid?, "Should be valid"
     end
   end
+
+  context "has validations" do
+    test "should be valid with all default values" do
+      assert FactoryGirl.build(:user).valid?
+    end
+
+    test "should have an email address" do
+      assert FactoryGirl.build(:user)
+      refute FactoryGirl.build(:user, email: "").valid?
+    end
+
+    test "should have a password" do
+      assert FactoryGirl.build(:user).valid?
+      refute FactoryGirl.build(:user, password: "").valid?
+    end
+  end
+
+  test "should return a specific user" do
+    user1 = FactoryGirl.create(:user)
+    user2 = FactoryGirl.create(:user, :email => Faker::Internet.email)
+    assert_equal user1, User.by_id(user1.id)
+  end
+
+  context "points" do
+    # not implemented
+    xtest "test should return a user's score" do
+    end
+
+    test "should return a user's rank" do
+      user = FactoryGirl.create(:user)
+      point = FactoryGirl.create(
+        :point,
+        :to_user_id => user.id)
+
+      assert_equal "B", user.rank
+    end
+
+    test "it should return a user's points" do
+      user = FactoryGirl.create(:user)
+      point = FactoryGirl.create(
+        :point,
+        :to_user_id => user.id)
+
+      assert_equal 5000, user.points
+    end
+  end
 end
