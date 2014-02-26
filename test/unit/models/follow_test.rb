7ace29fc4373a10867f83fa048de5ffefd717f35
Michael Hayman <michael.hayman@zoocasa.com>
2014-02-26 03:33:36 -0500

    Convert Follow specs to tests.

--- test/unit/models/follow_test.rb
+++ test/unit/models/follow_test.rb
@@ -1,15 +1,14 @@
-require 'spec_helper'
+require 'test_helper'
 
-describe Follow do
+class FollowTest < DoGood::TestCase
   context "has validations" do
   end
 
   # This is implemented by each controller it seems
-  pending "returns followings for a given type and instance" do
+  xtest "returns followings for a given type and instance" do
     user = FactoryGirl.create(:user)
     good = FactoryGirl.create(:good)
     user.follow(good)
     Follow.following("User", 1).should have(1).items
   end
 end
-
