bb2dfac21e5af2e72c796b80d4bd204ba464d362
Michael Hayman <michael.hayman@zoocasa.com>
2014-02-26 03:19:19 -0500

    Convert Comment specs to tests.

---
+++ test/unit/models/comment_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class CommentTest < DoGood::TestCase
+  context "has validations" do
+    test "should be valid with all default values" do
+      assert FactoryGirl.build(:comment).valid?
+    end
+
+    test "should have a message" do
+      assert FactoryGirl.build(:comment).valid?
+      refute FactoryGirl.build(:comment, :comment => "").valid?
+    end
+
+    test "should not be too long" do
+      assert FactoryGirl.build(:comment).valid?
+      refute FactoryGirl.build(:comment, :too_long).valid?
+    end
+
+    test "should have a user" do
+      assert FactoryGirl.build(:comment, user_id: 5).valid?
+      refute FactoryGirl.build(:comment, user_id: "").valid?
+    end
+
+    test "should not be too short" do
+      refute FactoryGirl.build(:comment, :too_short).valid?
+    end
+
+    context "arrays" do
+      test "should return all comments for a specific good" do
+        good = FactoryGirl.create(:good)
+
+        FactoryGirl.create(
+          :comment,
+          :commentable_id => good.id,
+          :commentable_type => "Good")
+        FactoryGirl.create(
+          :comment,
+          :commentable_id => good.id,
+          :commentable_type => "Good")
+
+        assert_equal 2, Comment.for_good(good.id).count
+      end
+    end
+  end
+end
