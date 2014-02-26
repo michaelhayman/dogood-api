1dc3f4e87c9af79d1b854a046482ed606d1da972
Michael Hayman <michael.hayman@zoocasa.com>
2014-02-26 03:41:03 -0500

    Convert Claimed Reward specs to tests.

--- test/unit/models/claimed_reward_test.rb
+++ test/unit/models/claimed_reward_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
-class ClaimedRewardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class ClaimedRewardTest < DoGood::TestCase
+  xtest "validations"
+  xtest "test claimed rewards"
+
+  context "has validations" do
+    test "should withdraw points" do
+      assert FactoryGirl.build(:claimed_reward).valid?
+    end
+  end
+
+  context "point management" do
+    test "should withdraw points" do
+      user = FactoryGirl.create(:user)
+      point = FactoryGirl.create(
+        :point,
+        :to_user_id => user.id)
+
+      points = user.points
+
+      claimed_reward = FactoryGirl.create(
+        :claimed_reward,
+        :user_id => user.id)
+      claimed_reward.withdraw_points
+
+      assert_equal user.points, points - claimed_reward.reward.cost
+    end
+  end
 end
