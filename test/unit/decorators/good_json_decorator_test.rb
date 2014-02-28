2244afc3733e3b07b4cdcd7448b247c5c7b5c443
Michael Hayman <michael.hayman@zoocasa.com>
2014-02-27 19:15:17 -0500

    Prettify Good JSON Decorator.

--- test/unit/decorators/good_json_decorator_test.rb
+++ test/unit/decorators/good_json_decorator_test.rb
@@ -1,7 +1,5 @@
 require 'test_helper'
 
-# !", "comments"=>[], "comments_count"=>0, "created_at"=>"2013-01-01T05:00:00.000Z", "current_user_commented"=>true, "current_user_liked"=>true, "current_user_regooded"=>false, "done"=>true, "entities"=>[], "evidence"=>nil, "id"=>12605, "lat"=>43.652527, "likes_count"=>0, "lng"=>-79.381961, "location_image"=>nil, "location_name"=>nil, "nominee"=>{"id"=>13549, "full_name"=>"Michael Hayman", "email"=>"ashlee@steuber.info", "phone"=>nil, "user_id"=>nil, "twitter_id"=>nil, "facebook_id"=>nil, "avatar"=>{"url"=>nil}}, "regoods_count"=>0, "user"=>{"id"=>14458, "email"=>"ana@towne.com", "created_at"=>"2013-01-01T05:00:00.000Z", "updated_at"=>"2013-01-01T05:00:00.000Z", "avatar"=>{"url"=>nil}, "follows_count"=>0, "full_name"=>"Tony", "phone"=>nil, "points_cache"=>0, "location"=>nil, "biography"=>nil, "facebook_id"=>nil, "twitter_id"=>nil}}
-
 class GoodJSONDecoratorTest < DoGood::TestCase
   def expected_hash
     good = @good.object
@@ -22,12 +20,12 @@ class GoodJSONDecoratorTest < DoGood::TestCase
       "lat" => @good.lat,
       "location_image" => @good.location_name,
       "location_name" => @good.location_name,
-      "nominee"=> {
+      "nominee" => {
         "user_id" => @good.nominee.user_id,
         "full_name" => @good.nominee.full_name
       },
-      "regoods_count"=> @good.regoods_count,
-      "user"=> {
+      "regoods_count" => @good.regoods_count,
+      "user" => {
         "id"=> good.user.id,
         "full_name" => good.user.full_name
       }
