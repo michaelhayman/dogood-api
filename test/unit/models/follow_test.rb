0c82e964e3b3ca3769e8ee9069acd6b3195333c2
Michael Hayman <michael.hayman@zoocasa.com>
2014-02-27 19:17:26 -0500

    Add a test for follow model.

--- test/unit/models/follow_test.rb
+++ test/unit/models/follow_test.rb
@@ -6,9 +6,16 @@ class FollowTest < DoGood::TestCase
 
   # This is implemented by each controller it seems
   xtest "returns followings for a given type and instance" do
-    user = FactoryGirl.create(:user)
-    good = FactoryGirl.create(:good)
-    user.follow(good)
-    Follow.following("User", 1).should have(1).items
+    @user = FactoryGirl.create(:user)
+    @good = FactoryGirl.create(:good)
+    @user.follow @good
+    Follow.following("Good", @good.id).should have(1).items
+  end
+
+  test "block" do
+    @follow = FactoryGirl.create(:follow)
+    refute @follow.blocked
+    @follow.block!
+    assert @follow.blocked
   end
 end
