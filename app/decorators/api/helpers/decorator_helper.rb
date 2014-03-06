78bb4214ae745db3a089dbc8a93d2ba60be66771
Michael Hayman <michael.hayman@zoocasa.com>
2014-03-06 08:03:47 -0500

    Remove unused methods from decorator helper.

--- app/decorators/api/helpers/decorator_helper.rb
+++ app/decorators/api/helpers/decorator_helper.rb
@@ -1,5 +1,3 @@
-# encoding: UTF-8
-
 module Api::Helpers::DecoratorHelper
   extend ActiveSupport::Concern
 
@@ -7,22 +5,6 @@ module Api::Helpers::DecoratorHelper
     extend Memoist
 
     delegate_all
-
-    memoize :output_attributes
   end
-
-  private
-    def output_attributes
-      if context[:output_attributes].present?
-        Array.wrap(context[:output_attributes]).each_with_object(HashWithIndifferentAccess.new) do |attr, memo|
-          memo[attr] = true
-        end
-      end
-    end
-
-    def output_optional_attribute?(name)
-      output_attributes.blank? ||
-        !!output_attributes.try(:[], name)
-    end
 end
 
