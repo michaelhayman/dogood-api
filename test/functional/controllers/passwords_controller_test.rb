02ee290a8ca2ae7ddb2e2b845a6c7cd832704fc7
Michael Hayman <michael.hayman@zoocasa.com>
2014-03-06 08:41:43 -0500

    Fix passwords test.

--- test/functional/controllers/passwords_controller_test.rb
+++ test/functional/controllers/passwords_controller_test.rb
@@ -8,14 +8,33 @@ class PasswordsControllerTest < DoGood::ActionControllerTestCase
       super
 
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      @user = FactoryGirl.build(:user, :bob)
+      @user = FactoryGirl.create(:user, :bob)
     end
 
-    test "should fail for a non-existing email address" do
+    test "should allow access with the right parameters" do
       post :create, {
         format: :json,
         user: {
           email: @user.email
+        }
+      }
+      assert_response :success
+    end
+
+    test "route" do
+      assert_routing '/users/1', {
+        controller: "users",
+        action: "show",
+        id: "1"
+      }
+    end
+
+    test "should fail for a non-existing email address" do
+      @non_user = FactoryGirl.build(:user, :tony)
+      post :create, {
+        format: :json,
+        user: {
+          email: @non_user.email
         }
       }
       json = jsonify(response)
@@ -24,7 +43,6 @@ class PasswordsControllerTest < DoGood::ActionControllerTestCase
     end
 
     test "should be successful for an existing email address" do
-      @user = FactoryGirl.create(:user, :bob)
       post :create, {
         format: :json,
         user: {
