5543c267672b91596f8c67c869e2dab71c71c653
Michael Hayman <michael.hayman@zoocasa.com>
2014-03-06 05:58:54 -0500

    Add colours to category factories.

--- test/factories/category.rb
+++ test/factories/category.rb
@@ -1,13 +1,16 @@
 FactoryGirl.define do
   factory :category do
     name "Environment"
+    colour "c4c622"
 
     trait :health do
       name "Health"
+      colour "d52a25"
     end
 
     trait :care do
       name "Care"
+      colour "b33977"
     end
   end
 end
