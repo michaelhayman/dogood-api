a2ce81d86e68768b0760a7b3328f3af8ea5004bd
Michael Hayman <michael.hayman@zoocasa.com>
2014-03-06 05:57:19 -0500

    Remove category serializer from model.

--- app/models/category.rb
+++ app/models/category.rb
@@ -21,16 +21,3 @@ class Category < ActiveRecord::Base
   end
 end
 
-class CategorySerializer < ActiveModel::Serializer
-  cached
-
-  attributes :id,
-    :name,
-    :name_constant,
-    :colour,
-    :image_url
-
-  def cache_key
-    object
-  end
-end
