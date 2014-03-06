3050629d2355ae976d9514cf06a46c9042a01688
Michael Hayman <michael.hayman@zoocasa.com>
2014-03-06 04:49:00 -0500

    Move good serializer into its own class & test.

---
+++ app/serializers/good_serializer.rb
@@ -0,0 +1,60 @@
+class GoodSerializer < ActiveModel::Serializer
+  def serializable_hash
+    defaults_serializer_hash.merge current_user_good_serializer_hash
+  end
+
+  private
+
+  def current_user_good_serializer_hash
+    CurrentUserGoodSerializer.new(object, options).serializable_hash
+  end
+
+  def defaults_serializer_hash
+    DefaultsSerializer.new(object, options).serializable_hash
+  end
+end
+
+class DefaultsSerializer < ActiveModel::Serializer
+
+  # cached
+
+  attributes :id,
+    :caption,
+    :likes_count,
+    :comments_count,
+    :regoods_count,
+    :lat,
+    :lng,
+    :location_name,
+    :location_image,
+    :evidence,
+    :done,
+    :created_at
+
+  has_many :comments, polymorphic: true
+  has_one :category
+  has_one :user
+  has_many :entities, :as => :entityable
+  has_one :nominee
+
+  def comments
+    object.comments.last(5)
+  end
+
+  def cache_key
+    [object, current_user]
+  end
+end
+
+class CurrentUserGoodSerializer < ActiveModel::Serializer
+  # cached
+
+  attributes :current_user_liked,
+    :current_user_commented,
+    :current_user_regooded
+
+  def cache_key
+    [object, current_user]
+  end
+end
+
---
+++ test/unit/serializers/good_serializer_test.rb
@@ -0,0 +1,81 @@
+require 'test_helper'
+
+class GoodSerializerTest < DoGood::TestCase
+  def expected_hash
+    @comment = @good.comments.first
+    @entity = @good.entities.first
+    {
+      goods: {
+        id: @good.id,
+        caption: @good.caption,
+        likes_count: @good.likes_count,
+        comments_count: @good.comments_count,
+        regoods_count: @good.regoods_count,
+        lat: @good.lat,
+        lng: @good.lng,
+        location_name: @good.location_name,
+        location_image: @good.location_image,
+        evidence: @good.evidence,
+        done: @good.done,
+        created_at: @good.created_at,
+        comments: [{
+          comment: @comment.comment,
+          created_at: @comment.created_at,
+          entities: @comment.entities,
+          user: {
+            id: @comment.user.id,
+            avatar_url: @comment.user.avatar_url,
+            full_name: @comment.user.full_name
+          },
+        }],
+        category: {
+          id: @good.category.id,
+          name: @good.category.name,
+          name_constant: @good.category.name_constant,
+          colour: @good.category.colour,
+          image_url: @good.category.image_url
+        },
+        user: {
+          id: @good.user.id,
+          avatar_url: @good.user.avatar_url,
+          full_name: @good.user.full_name
+        },
+        entities: [{
+          id: @entity.id,
+          link: @entity.link,
+          link_type: @entity.link_type,
+          link_id: @entity.link_id,
+          title: @entity.title,
+          range: @entity.range,
+          entityable_type: @entity.entityable_type,
+          entityable_id: @entity.entityable_id
+        }],
+        nominee: {
+          full_name: @good.nominee.full_name,
+          avatar_url: @good.nominee.avatar_url
+        },
+        current_user_liked: @good.current_user_liked,
+        current_user_commented: @good.current_user_commented,
+        current_user_regooded: false
+      }
+    }
+  end
+
+  def setup
+    super
+    @good = FactoryGirl.create(:good)
+    @entity = FactoryGirl.create(:entity, :entityable_id => @good.id, :entityable_type => "Good")
+    @comment = FactoryGirl.create(:comment, :commentable_id => @good.id, :user => @good.user)
+    @good = GoodDecorator.decorate(@good)
+    stub(@good).current_user_commented { true }
+    stub(@good).current_user_liked { true }
+    stub(@good).current_user_regooded { false }
+
+    @serializer = GoodSerializer.new @good, root: "goods"
+  end
+
+  test "json for api" do
+    assert_equal expected_hash.to_json, @serializer.to_json
+  end
+end
+
