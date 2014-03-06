9ac2bd4378c4ef2701a20d8548e09d336dacf41a
Michael Hayman <michael.hayman@zoocasa.com>
2014-03-06 04:57:20 -0500

    Update reports controller tests.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,18 @@
-class ReportsController < ApplicationController
+class ReportsController < ApiController
   before_filter :check_auth, only: [ :create ]
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    render_error(DoGood::Api::ParametersInvalid.new(exception.message))
+  end
 
   def create
     @report = Report.new(resource_params)
     @report.user_id = current_user.id
 
     if @report.save!
-      respond_with @report, root: "reports"
+      render json: @report, root: "reports"
     else
-      render_errors("Couldn't save the report. #{@report.errors}")
+      render_errors(DoGood::Api::RecordNotSaved.new("Couldn't save the report. #{@report.errors.full_messages.first}"))
     end
   end
 
--- test/functional/controllers/reports_controller_test.rb
+++ test/functional/controllers/reports_controller_test.rb
@@ -2,5 +2,57 @@ require 'test_helper'
 
 class ReportsControllerTest < DoGood::ActionControllerTestCase
   tests ReportsController
+
+  context "create" do
+    def setup
+      super
+
+      @user = FactoryGirl.create(:user, :bob)
+    end
+
+    test "should fail for an unauthorized user" do
+      post :create, {
+        format: :json,
+        user: {
+          email: @user.email
+        }
+      }
+      json = jsonify(response)
+
+      assert_response :unauthorized
+    end
+
+    test "should succeed with proper parameters" do
+      sign_in @user
+      @good = FactoryGirl.create(:good)
+
+      post :create, {
+        format: :json,
+        report: {
+          reportable_type: "Good",
+          reportable_id: @good.id
+        }
+      }
+      json = jsonify(response)
+
+      assert_response :success
+    end
+
+    test "should fail for a report that fails validations" do
+      sign_in @user
+      @good = FactoryGirl.create(:good)
+
+      post :create, {
+        format: :json,
+        report: {
+          reportable_type: "",
+          reportable_id: ""
+        }
+      }
+      json = jsonify(response)
+
+      assert_response :unprocessable_entity
+    end
+  end
 end
 
