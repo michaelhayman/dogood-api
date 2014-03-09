e7105715b33df4f6b1b715553dd57e55970bcbf7
Michael Hayman <michael.hayman@zoocasa.com>
2014-03-09 08:44:02 -0400

    Remove Fog mocking for now.

--- test/unit/decorators/user_decorator_test.rb
+++ test/unit/decorators/user_decorator_test.rb
@@ -4,10 +4,6 @@ class UserDecoratorTest < DoGood::TestCase
   def setup
     super
 
-    # Fog.mock!
-    # Fog.credentials_path = Rails.root.join('config/fog_credentials.yml')
-    # connection = Fog::Storage.new(:provider => 'AWS')
-    # connection.directories.create(:key => 'my_bucket')
     @user = FactoryGirl.create(:user).decorate
   end
 
@@ -19,11 +15,10 @@ class UserDecoratorTest < DoGood::TestCase
     @tony = FactoryGirl.create(:user, :tony)
     assert_equal 0, @user.followers_count
     @tony.follow @user.object
-    # p @user.follows_count
-    # @user.object.follow @tony
     assert_equal 1, @user.followers_count
   end
 
+  # since
   test "following_count" do
     @tony = FactoryGirl.create(:user, :tony)
     assert_equal 0, @user.following_count
@@ -45,6 +40,12 @@ class UserDecoratorTest < DoGood::TestCase
     assert_equal 1, @user.posted_or_followed_goods_count
     @user_good = FactoryGirl.create(:good, :user => @user)
     assert_equal 2, @user.posted_or_followed_goods_count
+  end
+
+  def teardown
+    super
+
+    @user.destroy
   end
 end
 
