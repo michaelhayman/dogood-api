d332eb4f710ab324f5f7304a151394086d8d4b32
Michael Hayman <michael.hayman@zoocasa.com>
2014-03-17 07:17:10 -0400

    Add Merit fields to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_merit
+
   include DoGood::Reportable
 
   devise :database_authenticatable,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317000206) do
+ActiveRecord::Schema.define(version: 20140317104458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -237,6 +237,8 @@ ActiveRecord::Schema.define(version: 20140317000206) do
     t.string   "biography"
     t.string   "facebook_id"
     t.string   "twitter_id"
+    t.integer  "sash_id"
+    t.integer  "level",                  default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
