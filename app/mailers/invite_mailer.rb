91002d5f052879f8e708c582443e9621bee2d9f9
Michael Hayman <michael@springbox.ca>
2014-05-16 16:40:29 -0400

    Guard against empty email address.

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -2,9 +2,11 @@ class InviteMailer < ActionMailer::Base
   def invite_nominee(nominee, nominator)
     @nominee = nominee
     @nominator = nominee
-    mail(:to => @nominee.email, :from => "invites@dogood.mobi", :subject => "You've been nominated at Do Good") do |format|
-      format.html
-      format.text
+    if @nominee.email.present?
+      mail(:to => @nominee.email, :from => "invites@dogood.mobi", :subject => "You've been nominated at Do Good") do |format|
+        format.html
+        format.text
+      end
     end
   end
 end
--- test/unit/mailers/invite_mailer_test.rb
+++ test/unit/mailers/invite_mailer_test.rb
@@ -15,6 +15,14 @@ class InviteMailerTest < DoGood::TestCase
     assert_match(/You've been nominated at Do Good/, @email.encoded)
     assert_equal [ @nominee.email ], @email.to
   end
+
+  test "don't send invite if no email address is present" do
+    nominee = @nominee
+    nominee.email = nil
+    @email = InviteMailer.invite_nominee(nominee, @nominator).deliver
+
+    assert ActionMailer::Base.deliveries.empty?
+  end
 end
 
 
