f5e9c69507f4237cda4b38a5a75117540637b2ac
Michael Hayman <michael@springbox.ca>
2014-06-01 08:15:20 -0400

    Add mailcatcher configuration.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,8 @@ DoGoodApp::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   config.action_mailer.default_url_options = { :host => 'localhost', :port => 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
   config.assets.compile = false
   config.assets.precompile = false
