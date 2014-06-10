b4c7632ba2cb081982bfcbdcb45ead072477f0fa
Michael Hayman <michael@springbox.ca>
2014-06-09 23:23:53 -0400

    Add rewards seeds.

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,7 +1,9 @@
 include Sprig::Helpers
 
 Category.destroy_all
+Reward.destroy_all
 
 ActiveRecord::Base.connection.reset_pk_sequence!(Category.table_name)
+ActiveRecord::Base.connection.reset_pk_sequence!(Reward.table_name)
 
-sprig [Category]
+sprig [ Category, Reward ]
