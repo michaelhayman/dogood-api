262f5279a1a424087c90ad625a69d33cfada1673
Michael Hayman <michael@springbox.ca>
2014-06-15 20:42:09 -0400

    Manually increment and decrement following counts.
    Both Users and Goods are updated when a follow occurs on Good.
    
    We really just want Goods to be updated.

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,5 +1,4 @@
 class Follow < ActiveRecord::Base
-
   extend ActsAsFollower::FollowerLib
   extend ActsAsFollower::FollowScopes
 
@@ -8,10 +7,20 @@ class Follow < ActiveRecord::Base
   belongs_to :followable,
     polymorphic: true,
     counter_cache: :cached_followers_count
-  # add another counter_cache column
+
+  # polymorphic counter caches update both relationships,
+  # when we just want one.
   belongs_to :follower,
-    polymorphic: true,
-    counter_cache: :cached_following_count
+    polymorphic: true
+
+  after_create :increment_following_counters
+  after_destroy :decrement_following_counters
+
+  [:increment, :decrement].each do |type|
+    define_method("#{type}_following_counters") do
+      follower_type.classify.constantize.send("#{type}_counter", "cached_following_count".to_sym, self.follower_id) if followable_type == "User"
+    end
+  end
 
   def block!
     self.update_attribute(:blocked, true)
