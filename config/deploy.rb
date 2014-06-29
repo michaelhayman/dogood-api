b70f2da77c0119acbdebdf5515290c0d07e469cc
Michael Hayman <michael@springbox.ca>
2014-06-28 23:42:01 -0400

    Don't double bundle exec rake.

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,7 +74,7 @@ end
 def run_remote_rake(rake_cmd)
   rake_args = ENV['RAKE_ARGS'].to_s.split(',')
 
-  cmd = "cd #{fetch(:latest_release)} && bundle exec #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
+  cmd = "cd #{fetch(:latest_release)} && #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
   cmd += "['#{rake_args.join("','")}']" unless rake_args.empty?
   run cmd
   set :rakefile, nil if exists?(:rakefile)
