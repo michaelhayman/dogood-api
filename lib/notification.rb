eb86f5159e0ad35464a65c0528ce5544855c38ae
Michael Hayman <michael@springbox.ca>
2014-06-29 11:38:21 -0400

    Set constants in top level of notification method.

--- lib/notification.rb
+++ lib/notification.rb
@@ -1,15 +1,15 @@
 require 'houston'
 
 class Notification
-  def self.send_url(token, message, url)
-    if Rails.env.production?
-      APN = Houston::Client.production
-      APN.certificate = File.read("config/apple_push_notification_production.pem")
-    else
-      APN = Houston::Client.development
-      APN.certificate = File.read("config/apple_push_notification_development.pem")
-    end
+  if Rails.env.production?
+    APN = Houston::Client.production
+    APN.certificate = File.read("config/apple_push_notification_production.pem")
+  else
+    APN = Houston::Client.development
+    APN.certificate = File.read("config/apple_push_notification_development.pem")
+  end
 
+  def self.send_url(token, message, url)
     notif = Houston::Notification.new(device: token)
     notif.alert = message
     notif.badge = 0
