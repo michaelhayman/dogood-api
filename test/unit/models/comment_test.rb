8c757b0848d480eae66bcc8af6becc83f996467b
Michael Hayman <michael@springbox.ca>
2014-06-28 21:36:01 -0400

    Collapse Comment tests into one class.

--- test/unit/models/comment_test.rb
+++ test/unit/models/comment_test.rb
@@ -1,43 +1,39 @@
 class CommentTest < DoGood::TestCase
-  class CommentValidations < DoGood::TestCase
-    test "should be valid with all default values" do
-      assert FactoryGirl.build(:comment).valid?
-    end
-
-    test "should have a message" do
-      assert FactoryGirl.build(:comment).valid?
-      refute FactoryGirl.build(:comment, comment: "").valid?
-    end
-
-    test "should not be too long" do
-      assert FactoryGirl.build(:comment).valid?
-      refute FactoryGirl.build(:comment, :too_long).valid?
-    end
-
-    test "should have a user" do
-      assert FactoryGirl.build(:comment, user_id: 5).valid?
-      refute FactoryGirl.build(:comment, user_id: "").valid?
-    end
-
-    test "should not be too short" do
-      refute FactoryGirl.build(:comment, :too_short).valid?
-    end
+  test "should be valid with all default values" do
+    assert FactoryGirl.build(:comment).valid?
   end
 
-  class CommentArrays < DoGood::TestCase
-    test "should return all comments for a specific good" do
-      good = FactoryGirl.create(:good)
+  test "should have a message" do
+    assert FactoryGirl.build(:comment).valid?
+    refute FactoryGirl.build(:comment, comment: "").valid?
+  end
 
-      FactoryGirl.create(
-        :comment,
-        commentable_id: good.id,
-        commentable_type: "Good")
-      FactoryGirl.create(
-        :comment,
-        commentable_id: good.id,
-        commentable_type: "Good")
+  test "should not be too long" do
+    assert FactoryGirl.build(:comment).valid?
+    refute FactoryGirl.build(:comment, :too_long).valid?
+  end
 
-      assert_equal 2, Comment.for_good(good.id).count
-    end
+  test "should have a user" do
+    assert FactoryGirl.build(:comment, user_id: 5).valid?
+    refute FactoryGirl.build(:comment, user_id: "").valid?
+  end
+
+  test "should not be too short" do
+    refute FactoryGirl.build(:comment, :too_short).valid?
+  end
+
+  test "should return all comments for a specific good" do
+    good = FactoryGirl.create(:good)
+
+    FactoryGirl.create(
+      :comment,
+      commentable_id: good.id,
+      commentable_type: "Good")
+    FactoryGirl.create(
+      :comment,
+      commentable_id: good.id,
+      commentable_type: "Good")
+
+    assert_equal 2, Comment.for_good(good.id).count
   end
 end
