306e6d99c85c0148bdd66754c070d0a3c9fba35e
Michael Hayman <michael@springbox.ca>
2014-07-01 14:18:57 -0400

    Don't send more than one identical notification.

--- app/workers/notifier_worker.rb
+++ app/workers/notifier_worker.rb
@@ -8,6 +8,8 @@ class NotifierWorker
     APNConnection.new
   end
 
+  REDIS = Redis.new
+
   def perform(message, recipient_ids, custom_data = nil)
     recipient_ids = Array(recipient_ids)
 
@@ -15,11 +17,18 @@ class NotifierWorker
       tokens = User.where(id: recipient_ids).collect {|u| u.devices.collect(&:token)}.flatten
 
       tokens.each do |token|
-        notification = Houston::Notification.new(device: token)
-        notification.alert = message
-        notification.sound = 'default'
-        notification.custom_data = custom_data
-        connection.write(notification.message)
+        digest = Digest::MD5.hexdigest("#{message}#{token}#{custom_data}")
+        if REDIS.get(digest) != "sent"
+          notification = Houston::Notification.new(device: token)
+          notification.alert = message
+          notification.sound = 'default'
+          notification.custom_data = custom_data
+          connection.write(notification.message)
+          REDIS.set(digest, "sent")
+          Rails.logger.info "#{digest} created."
+        else
+          Rails.logger.info "Already sent notification for this event."
+        end
       end
     end
   end
