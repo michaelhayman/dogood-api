94d0e42f3b01215af8fe5f0d84ea927704cdf9c8
Michael Hayman <michael@springbox.ca>
2014-07-01 14:32:31 -0400

    Add test for vote notifications.

--- test/unit/models/vote_test.rb
+++ test/unit/models/vote_test.rb
@@ -18,4 +18,14 @@ class VoteTest < DoGood::TestCase
   def build_vote
     FactoryGirl.create(:vote, :good_like, voter_id: @user.id, votable_id: @good.id)
   end
+
+  test "send notification for vote" do
+    @good = FactoryGirl.create(:good)
+    @bob = FactoryGirl.create(:user)
+    stub(NotifierWorker).perform_async { true }
+    Vote.send_notification(@good, @bob)
+    message = "#{@user.full_name} voted on your post"
+    url = "dogood://goods/#{@good.id}"
+    assert_received(NotifierWorker) { |o| o.perform_async(message, @good.user_id, { url: url }) }
+  end
 end
