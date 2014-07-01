25025001820e419875f28021607b84698347c932
Michael Hayman <michael@springbox.ca>
2014-07-01 11:11:18 -0400

    Prevent infinite loop on Unicorn startup on production.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,9 +7,4 @@ stdout_path "#{root}/log/unicorn.log"
 listen "/tmp/unicorn.dg-api.sock"
 worker_processes 2
 timeout 30
-
-# Force the bundler gemfile environment variable to
-# reference the capistrano "current" symlink
-before_exec do |_|
-  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
-end
+preload_app true
