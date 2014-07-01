44e5c113ba2548e0210e5180dace08bf466e5c67
Michael Hayman <michael@springbox.ca>
2014-07-01 11:09:47 -0400

    Link sidekiq configuration to correct directory.

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ namespace :deploy do
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-available/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    sudo "ln -nfs #{current_path}/config/sidekiq.conf /etc/init.d/sidekiq.conf"
+    sudo "ln -nfs #{current_path}/config/sidekiq.conf /etc/init/sidekiq.conf"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.vps.yml"), "#{shared_path}/config/database.yml"
     put File.read("config/apple_push_notification_production.pem"), "#{shared_path}/config/apple_push_notification_production.pem"
