b6ea74808a327042fae0c742bfa5b7a6523e495f
Michael Hayman <michael@springbox.ca>
2014-12-11 22:18:52 -0500

    Make 'vote' work.

--- app/assets/javascripts/goods.js
+++ app/assets/javascripts/goods.js
@@ -5,10 +5,36 @@ var goods = {
 
   attachEvents: function() {
     $(document).on('click', '.d-comment-link', function() {
-      debugger;
     });
     $(document).on('click', '.d-vote-link', function() {
-      debugger;
+      var method, data, url;
+
+      data = {
+        vote: {
+          votable_id: $(this).data('id'),
+          votable_type: "Good"
+        }
+      };
+
+      if ($(this).data('state') === "on") {
+        method = "DELETE";
+        url = '/votes/' + $(this).data('id');
+      } else {
+        method = "POST";
+        url = '/votes';
+      }
+
+      $.ajax({
+        url: url,
+        data: data,
+        method: method
+      }).done(function(response, statusCode, xhr) {
+        debugger;
+        location.reload();
+      }).fail(function(xhr, statusCode, errorThrown) {
+        debugger;
+        // userSession.showError(userSession.errorMessage(xhr));
+      });
     });
     $(document).on('click', '.d-follow-link', function() {
       debugger;
