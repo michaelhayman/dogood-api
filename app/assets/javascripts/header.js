25800a39676811b88d8ddf3f049572cbfdf0eeff
Michael Hayman <michael@springbox.ca>
2014-12-13 01:06:50 -0500

    Streamline sign in button.

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,12 +1,8 @@
 
 var header = {
   init: function() {
-    $(document).on('click', '.d-profile-icon', function() {
+    $(document).on('click', '.d-open-user-session-dialog-link', function() {
       $(document).triggerHandler('needsSignIn.APP');
-    });
-
-    $(document).on('click', '.d-post-icon', function() {
-      console.log('hey');
     });
   }
 }
