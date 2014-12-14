955647ff80f97139f16eccf2d6e7aac50a5fdd66
Michael Hayman <michael@springbox.ca>
2014-12-14 02:28:45 -0500

    Set assets to compress.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,7 @@ DoGoodApp::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.compress = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
