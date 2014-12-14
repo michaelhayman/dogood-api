96d3ce7268a8108bd4e0f4cd39647f17fd222515
Michael Hayman <michael@springbox.ca>
2014-12-13 23:23:27 -0500

    Add more options to individual good partial.
    * Showing and hiding the comment box depending on where you're
      viewing it
    * Displaying additional comments on good show page

--- app/helpers/goods_helper.rb
+++ app/helpers/goods_helper.rb
@@ -1,4 +1,24 @@
 module GoodsHelper
+  def show_page?
+    params[:action] = "show" && params[:controller] == "goods"
+  end
+
+  def comments_to_show
+    if show_page?
+      100
+    else
+      5
+    end
+  end
+
+  def comment_box_display
+    if show_page?
+      "block"
+    else
+      "none"
+    end
+  end
+
   def good_collection(goods)
     render partial: 'goods/individual', collection: goods, as: :good
   end
