e2111b8a8ebfc7dfb87ac3374c33b70cc30a1f04
Michael Hayman <michael@springbox.ca>
2014-12-14 00:02:12 -0500

    Add iTunes meta tag.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def ios_app_equivalent_url
+    if params[:controller] == "goods"
+      if params[:action] == "show"
+        "goods/#{params[:id]}"
+      elsif params[:action] == "new"
+        "goods/new"
+      end
+    elsif params[:controller] == "users"
+      if params[:action] == "show"
+        "users/#{params[:id]}"
+      end
+    end
+  end
 end
